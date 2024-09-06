4f678290828311a0fc175a0f5dc602ae68f24f89
Mizu <psdk55052004@gmail.com>
2024-09-06 14:43:04 +0800

    優化 D1

--- day1-10/day2/index.js
+++ day1-10/day2/index.js
@@ -5,31 +5,16 @@ let menuWrapper = null
 document.addEventListener('DOMContentLoaded', () => {
   menuWrapper = document.querySelector('.menu-wrapper')
   if (menuWrapper) menuWrapper.addEventListener('click', menuToggle)
-
-  //! 測試
-  menuWrapper.classList.toggle('active')
-  // menuWrapper.classList.add('line-cross-1-pre')
-  
-  setTimeout(() => {
-    menuWrapper.classList.toggle('active')
-    menuWrapper.classList.toggle('line-cross-1-pre')
-    // setTimeout(() => {
-    //   // menuWrapper.classList.toggle('active')
-    //   menuWrapper.classList.remove('line-cross-1-pre')
-    // }, 4500)
-  }, 1000)
 })
 
 
 function menuToggle () {
-  const line1 = document.querySelector('.line-1')
-  const line2 = document.querySelector('.line-2')
-  const line3 = document.querySelector('.line-3')
-  // if (line1 && line2 && line3) {
-  //   line1.classList.remove('no-animation')
-  //   line2.classList.remove('no-animation')
-  //   line3.classList.remove('no-animation')
-  // }
-  if (menuWrapper) menuWrapper.classList.toggle('active')
+  if (menuWrapper) {
+    const childrens = [...menuWrapper.children]
+    childrens.forEach(el => el.classList.remove('no-animatetion'))
+
+    menuWrapper.classList.toggle('active')
+  }
 }
+
 
