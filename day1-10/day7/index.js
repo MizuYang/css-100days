f4e11d9467516d281567687d3dfb2f8ffad8c37e
Mizu <psdk55052004@gmail.com>
2024-09-11 14:29:33 +0800

    新增 D7: 搜尋框顯示/隱藏、動畫效果

--- day1-10/day7/index.js
+++ day1-10/day7/index.js
@@ -7,15 +7,19 @@
 const btnMenu = document.querySelector('.btn-menu')
 const container = document.querySelector('.container')
 const menu = document.querySelector('.menu')
+const btnSearch = document.querySelector('.btn-search')
+const searchInput = document.querySelector('.search-input')
 
 btnMenu.addEventListener('click', menuToggle)
-
-
-
-
-
+btnSearch.addEventListener('click', searchInputToggle)
 
 function menuToggle () {
   container.classList.toggle('menu-show')
   menu.classList.toggle('menu-show')
+}
+function searchInputToggle () {
+  searchInput.classList.toggle('search-input-show')
+  if(searchInput.classList.contains('search-input-show')) {
+    searchInput.focus()
+  }
 }
