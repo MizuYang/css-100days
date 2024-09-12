ff55a638836af274a2a4f27dda0d0ad37d74e126
Mizu <psdk55052004@gmail.com>
2024-09-12 14:27:03 +0800

    新增 D13: 門 動畫

--- day11-20/day13/index.js
+++ day11-20/day13/index.js
@@ -11,7 +11,14 @@
 
 
 
+const masks = [...document.querySelectorAll('.mask')]
+const doorTop = document.querySelector('.door-top')
+const doorBottom = document.querySelector('.door-bottom')
 
+masks.forEach(el => el.addEventListener('click', doorShowToggle))
 
-
+function doorShowToggle() {
+  doorTop.classList.toggle('show')
+  doorBottom.classList.toggle('show')
+}
 
