ae452f583f75b1c92163ff08c449d55ef30fac7b
Mizu <psdk55052004@gmail.com>
2024-09-12 15:53:12 +0800

    新增 D13: 個人訊息區塊

--- day11-20/day13/index.js
+++ day11-20/day13/index.js
@@ -14,11 +14,16 @@
 const masks = [...document.querySelectorAll('.mask')]
 const doorTop = document.querySelector('.door-top')
 const doorBottom = document.querySelector('.door-bottom')
+const btnClose = document.querySelector('.btn-close')
+const info = document.querySelector('.info')
 
 masks.forEach(el => el.addEventListener('click', doorShowToggle))
+
+btnClose.addEventListener('click', doorShowToggle)
 
 function doorShowToggle() {
   doorTop.classList.toggle('show')
   doorBottom.classList.toggle('show')
+  info.classList.toggle('show')
 }
 
