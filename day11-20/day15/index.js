42bb5d06315b3f9bd7056fba0c30dd70bb4b8e17
Mizu <psdk55052004@gmail.com>
2024-09-13 15:44:22 +0800

    新增 D15: 進度條

--- day11-20/day15/index.js
+++ day11-20/day15/index.js
@@ -10,23 +10,40 @@
 const btnUpload = document.querySelector('.btn-upload')
 const uploadInput = document.querySelector('.upload-input')
 const uploadLabel = document.querySelector('.upload-label')
+const uploadFileName = document.querySelector('.upload-file-name')
 
 
 btnUpload.addEventListener('click', upload)
+uploadInput.addEventListener('change', upload)
 
-function upload () {
-  console.log('uploadInput', uploadInput.value)
+function upload (e) {
+  checkFile(e)
+  uploading()
+}
+
+function checkFile (e) {
   if (uploadInput.value) {
+    if (e.target.files) {
+      uploadFileName.textContent = e.target.files[0].name
+    }
     btnUpload.textContent = 'Upload file'
-    btnUpload.classList.remove('is-invalid')
+  
+    uploadLabel.classList.add('is-valid')
+    
     uploadLabel.classList.remove('is-invalid')
+    btnUpload.classList.remove('is-invalid')
   } else {
+    uploadFileName.textContent = '未上傳任何檔案'
     btnUpload.textContent = 'No file selected!'
     btnUpload.classList.add('is-invalid')
     uploadLabel.classList.add('is-invalid')
+  
+    uploadLabel.classList.remove('is-valid')
   }
-
-    // No file selected!
 }
-
-
+function uploading () {
+  
+  setTimeout(() => {
+    
+  }, 3000);
+}
