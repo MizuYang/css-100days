616fcdd6de30826f6a4c75e95c94884c93513fe5
Mizu <psdk55052004@gmail.com>
2024-09-13 15:52:36 +0800

    新增 D15: 上傳檔案後跑進度條動畫

--- day11-20/day15/index.js
+++ day11-20/day15/index.js
@@ -11,17 +11,19 @@ const btnUpload = document.querySelector('.btn-upload')
 const uploadInput = document.querySelector('.upload-input')
 const uploadLabel = document.querySelector('.upload-label')
 const uploadFileName = document.querySelector('.upload-file-name')
+const progressBar = document.querySelector('.progress-bar')
 
 
 btnUpload.addEventListener('click', upload)
-uploadInput.addEventListener('change', upload)
+uploadInput.addEventListener('change', checkFile)
 
 function upload (e) {
-  checkFile(e)
-  uploading()
+  const result = checkFile(e)
+  if (result) uploading()
 }
 
 function checkFile (e) {
+  let result = false
   if (uploadInput.value) {
     if (e.target.files) {
       uploadFileName.textContent = e.target.files[0].name
@@ -32,6 +34,7 @@ function checkFile (e) {
     
     uploadLabel.classList.remove('is-invalid')
     btnUpload.classList.remove('is-invalid')
+    result = true
   } else {
     uploadFileName.textContent = '未上傳任何檔案'
     btnUpload.textContent = 'No file selected!'
@@ -39,11 +42,10 @@ function checkFile (e) {
     uploadLabel.classList.add('is-invalid')
   
     uploadLabel.classList.remove('is-valid')
+    result = false
   }
+  return result
 }
 function uploading () {
-  
-  setTimeout(() => {
-    
-  }, 3000);
+  progressBar.classList.add('is-valid')
 }
