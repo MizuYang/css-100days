2e514445e639dfbf84ccf3e86dc56467f621cbf8
Mizu <psdk55052004@gmail.com>
2024-09-13 14:49:02 +0800

    新增 D15: 上傳按鈕 搖晃動畫效果

--- day11-20/day15/index.js
+++ day11-20/day15/index.js
@@ -9,6 +9,7 @@
 
 const btnUpload = document.querySelector('.btn-upload')
 const uploadInput = document.querySelector('.upload-input')
+const uploadLabel = document.querySelector('.upload-label')
 
 
 btnUpload.addEventListener('click', upload)
@@ -16,11 +17,13 @@ btnUpload.addEventListener('click', upload)
 function upload () {
   console.log('uploadInput', uploadInput.value)
   if (uploadInput.value) {
-    btnUpload.classList.remove('is-invalid')
     btnUpload.textContent = 'Upload file'
+    btnUpload.classList.remove('is-invalid')
+    uploadLabel.classList.remove('is-invalid')
   } else {
-    btnUpload.classList.add('is-invalid')
     btnUpload.textContent = 'No file selected!'
+    btnUpload.classList.add('is-invalid')
+    uploadLabel.classList.add('is-invalid')
   }
 
     // No file selected!
