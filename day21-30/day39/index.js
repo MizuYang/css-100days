a2c6a598067cba29a114480730d8f0d36aeec626
Mizu <psdk55052004@gmail.com>
2024-09-23 10:22:59 +0800

    新增 animation 初始化

--- day21-30/day39/index.js
+++ day21-30/day39/index.js
@@ -4,9 +4,15 @@
 
 
 const checkbox = document.querySelector('.checkbox')
+const menu = document.querySelector('.menu')
 const links = [...document.querySelectorAll('.menu a')]
 
 links.forEach(el => el.addEventListener('click', menuToggle))
+menu.addEventListener('click', removeAnimationNone)
+
+function removeAnimationNone () {
+  if (menu) menu.classList.remove('animation-none')
+}
 
 function menuToggle () {
   if (checkbox) checkbox.checked = !checkbox.checked
