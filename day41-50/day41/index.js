0f59b62eaf0c630f547abce841e6324d89fa4c0e
Mizu <psdk55052004@gmail.com>
2024-09-24 08:18:03 +0800

    新增 D41: 點關閉按鈕縮小隱藏功能

--- day41-50/day41/index.js
+++ day41-50/day41/index.js
@@ -1,3 +1,13 @@
 
 
 
+
+
+const card = document.querySelector('.card')
+const btnClose = document.querySelector('.btn-close')
+
+btnClose.addEventListener('click', cardHide)
+
+function cardHide () {
+  if (card) card.classList.add('hide')
+}
